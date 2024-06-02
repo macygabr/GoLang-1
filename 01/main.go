73d95642d8ea754e541d4e09d3a0e3fbecb9ce97
macygabr <macygabr@student.21-school.ru>
2024-06-02 12:17:25 +0700

    backup

---
+++ 01/main.go
@@ -0,0 +1,27 @@
+package main
+
+type Human struct {
+	yers int
+	name string
+}
+
+func (*Human) move() {
+	println("Human move")
+}
+
+type Action struct {
+	man Human
+	act bool
+}
+
+func (a *Action) move() {
+	a.man.move()
+}
+
+func main() {
+	man := Human{}
+	man.move()
+
+	act := Action{}
+	act.move()
+}
