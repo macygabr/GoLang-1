5bcacbd9d0a328f3c43d4746d8d311c0ec7e8c6f
macygabr <macygabr@student.21-school.ru>
2024-06-02 13:27:01 +0700

    backup

--- 02/main.go
+++ 02/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+type cont struct {
+	arr [5]int
+}
+
+func main() {
+	var cont cont
+
+	cont.arr = [5]int{2, 4, 6, 8, 10}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < len(cont.arr); i++ {
+		wg.Add(1)
+		go cont.spinner(&wg, 1000*time.Millisecond, i)
+	}
+
+	wg.Wait()
+	fmt.Print(cont.arr)
+}
+
+func (cont *cont) spinner(wg *sync.WaitGroup, delay time.Duration, index int) {
+	defer wg.Done()
+	fmt.Println("Start ", index)
+	time.Sleep(delay)
+	cont.arr[index] = cont.arr[index] * cont.arr[index]
 }
--- 03/main.go
+++ 03/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+type cont struct {
+	arr [5]int
+	sum int
+}
+
+func main() {
+	cont := cont{
+		arr: [5]int{2, 4, 6, 8, 10},
+		sum: 0,
+	}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < len(cont.arr); i++ {
+		wg.Add(1)
+		go cont.spinner(&wg, 1000*time.Millisecond, i)
+	}
+
+	wg.Wait()
+	fmt.Println(cont.sum)
+}
+
+func (cont *cont) spinner(wg *sync.WaitGroup, delay time.Duration, index int) {
+	defer wg.Done()
+	fmt.Println("Start ", index)
+	time.Sleep(delay)
+	cont.arr[index] = cont.arr[index] * cont.arr[index]
+	cont.sum += cont.arr[index]
 }
