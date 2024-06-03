f38556c4430061b3606bbd66226abe147c836a8c
macygabr <macygabr@student.21-school.ru>
2024-06-03 21:41:19 +0700

    backup

--- 09/main.go
+++ 09/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"sync"
+)
 
+func main() {
+	var wg sync.WaitGroup
+	pack := make(chan int)
+
+	wg.Add(1)
+	go NurlanTheBest(&wg, &pack)
+
+	wg.Add(1)
+	go macygabr(&wg, &pack)
+
+	wg.Wait()
+}
+
+func NurlanTheBest(wg *sync.WaitGroup, pack *chan int) {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		(*pack) <- i
+	}
+}
+
+func macygabr(wg *sync.WaitGroup, pack *chan int) {
+	defer wg.Done()
+	// fmt.Println(<-pack)
 }
