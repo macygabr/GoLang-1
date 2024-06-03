2e2ebafc702c6ee957e9addf4019ddaf8d757391
macygabr <macygabr@student.21-school.ru>
2024-06-03 17:54:00 +0700

    08

--- 07/main.go
+++ 07/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	mymap := make(map[int]int)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go add(&wg, &mu, &mymap, i)
+	}
+
+	wg.Wait()
+	fmt.Println(mymap)
+}
+
+func add(wg *sync.WaitGroup, mu *sync.Mutex, mymap *map[int]int, i int) {
+	defer wg.Done()
+	defer mu.Unlock()
+
+	mu.Lock()
+	(*mymap)[i] = i
 }
--- 08/main.go
+++ 08/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var mask int64
+	var x int64
+	var i int
+	var byt int
+	mask = 1
+	x = 10000
+	i = 5
+	byt = 1
+
+	fmt.Printf("%064b\n", x)
+	mask = mask << i
+	fmt.Printf("%064b\n", mask)
+	if byt == 1 {
+		x = x | mask
+		fmt.Printf("|\n")
+	} else {
+		x = x &^ mask
+		fmt.Printf("&^\n")
+	}
+	fmt.Printf("%064b\n", x)
 }
